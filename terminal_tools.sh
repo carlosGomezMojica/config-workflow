4aefffa89b1aa950e9aaec423a4fb3a4c6d0fb22
Carlos Manuel Gomez Mojica <101277876+carlosGomezMojica@users.noreply.github.com>
2025-03-08 18:58:31 -0500

    Update terminal_tools.sh

--- terminal_tools.sh
+++ terminal_tools.sh
@@ -10,165 +10,122 @@ else
     exit 1
 fi
 
-# Función para instalar paquetes en Pop!_OS
-install_popos() {
-    sudo apt update
-    sudo apt install -y "$@"
+# Barra de progreso
+show_progress() {
+    local current=$1
+    local total=$2
+    local percent=$((current * 100 / total))
+    echo -ne "\r["
+    for ((i=0; i < percent / 2; i++)); do echo -n "#"; done
+    for ((i=percent / 2; i < 50; i++)); do echo -n "-"; done
+    echo -n "] $percent%"
 }
 
-# Función para instalar paquetes en Arch Linux
+TOTAL_STEPS=13
+CURRENT_STEP=0
+
+# Funciones de instalación
+install_popos() {
+    sudo apt update && sudo apt install -y "$@"
+}
+
 install_arch() {
     sudo pacman -Syu --noconfirm "$@"
 }
 
-# 1. Instalar Neovim y configurar
 install_nvim() {
-    if [ "$DISTRO" = 'popos' ]; then
-        install_popos neovim
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch neovim
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos neovim || install_arch neovim
     git clone https://github.com/carlosGomezMojica/starter ~/.config/nvim
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 2. Instalar Zsh
 install_zsh() {
-    if [ "$DISTRO" = 'popos' ]; then
-        install_popos zsh
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch zsh
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos zsh || install_arch zsh
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 3. Instalar Oh-My-Zsh y configurar
 install_oh_my_zsh() {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    RUNZSH=no CHSH=yes KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sed -i 's/^ZSH_THEME=.*/ZSH_THEME="passion"/' ~/.zshrc
 
     ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
-    # Instalar plugins
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-history-substring-search.git $ZSH_CUSTOM/plugins/zsh-history-substring-search
     git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
-
-    # Añadir plugins al .zshrc
-    sed -i 's/^plugins=(\(.*\))/plugins=(\1 zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search you-should-use)/' ~/.zshrc
-
-    # Configurar fzf-tab
     git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
+
+    sed -i 's/^plugins=(\(.*\))/plugins=(\1 zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search you-should-use)/' ~/.zshrc
     echo "source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh" >> ~/.zshrc
 
-    # Aplicar cambios
-    source ~/.zshrc
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 4. Instalar Docker
 install_docker() {
-    if [ "$DISTRO" = 'popos' ]; then
-        install_popos docker.io
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch docker
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos docker.io || install_arch docker
     sudo systemctl enable --now docker
     sudo usermod -aG docker $USER
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 5. Instalar LazyDocker
 install_lazydocker() {
-    if [ "$DISTRO" = 'popos' ]; then
-        sudo apt install -y golang-go
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch go
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos golang-go || install_arch go
     go install github.com/jesseduffield/lazydocker@latest
     sudo mv ~/go/bin/lazydocker /usr/local/bin/
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 6. Instalar LazyGit
 install_lazygit() {
-    if [ "$DISTRO" = 'popos' ]; then
-        sudo add-apt-repository ppa:lazygit-team/release
-        sudo apt update
-        sudo apt install -y lazygit
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch lazygit
-    fi
+    [ "$DISTRO" = 'popos' ] && { sudo add-apt-repository -y ppa:lazygit-team/release; install_popos lazygit; } || install_arch lazygit
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 7. Instalar Zoxide
 install_zoxide() {
-    if [ "$DISTRO" = 'popos' ]; then
-        curl -sS https://webinstall.dev/zoxide | bash
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch zoxide
-    fi
+    [ "$DISTRO" = 'popos' ] && curl -sS https://webinstall.dev/zoxide | bash || install_arch zoxide
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 8. Instalar tldr
 install_tldr() {
-    if [ "$DISTRO" = 'popos' ]; then
-        sudo npm install -g tldr
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch tldr
-    fi
+    [ "$DISTRO" = 'popos' ] && sudo npm install -g tldr || install_arch tldr
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 9. Instalar AWS CLI
 install_aws_cli() {
-    if [ "$DISTRO" = 'popos' ]; then
-        sudo apt install -y awscli
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch aws-cli
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos awscli || install_arch aws-cli
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 10. Instalar NVM
 install_nvm() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 11. Instalar PNPM
 install_pnpm() {
     curl -fsSL https://get.pnpm.io/install.sh | sh -
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 12. Instalar Zathura y configurar
 install_zathura() {
-    if [ "$DISTRO" = 'popos' ]; then
-        install_popos zathura
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch zathura
-    fi
-
-    CONFIG_DIR="$HOME/.config/zathura"
-    CONFIG_FILE="$CONFIG_DIR/zathurarc"
-
-    mkdir -p "$CONFIG_DIR"
-
-    cat > "$CONFIG_FILE" << EOL
-set default-bg       "#002b36"
-set default-fg       "#839496"
-set statusbar-bg     "#073642"
-set statusbar-fg     "#93a1a1"
-set notification-bg  "#073642"
-set notification-fg  "#93a1a1"
-set inputbar-bg      "#073642"
-set inputbar-fg      "#93a1a1"
-EOL
+    [ "$DISTRO" = 'popos' ] && install_popos zathura || install_arch zathura
+    mkdir -p ~/.config/zathura
+    cat > ~/.config/zathura/zathurarc << EOF
+set default-bg "#002b36"
+set default-fg "#839496"
+set statusbar-bg "#073642"
+set statusbar-fg "#93a1a1"
+set notification-bg "#073642"
+set notification-fg "#93a1a1"
+set inputbar-bg "#073642"
+set inputbar-fg "#93a1a1"
+EOF
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
-# 13. Instalar Kitty y configurar
 install_kitty() {
-    if [ "$DISTRO" = 'popos' ]; then
-        install_popos kitty
-    elif [ "$DISTRO" = 'arch' ]; then
-        install_arch kitty
-    fi
+    [ "$DISTRO" = 'popos' ] && install_popos kitty || install_arch kitty
     git clone https://github.com/carlosGomezMojica/kitty-solarized-osaka ~/.config/kitty
+    ((CURRENT_STEP++)); show_progress $CURRENT_STEP $TOTAL_STEPS
 }
 
 # Ejecutar funciones
@@ -176,6 +133,16 @@ install_nvim
 install_zsh
 install_oh_my_zsh
 install_docker
-install_l
-::contentReference[oaicite:0]{index=0}
+install_lazydocker
+install_lazygit
+install_zoxide
+install_tldr
+install_aws_cli
+install_nvm
+install_pnpm
+install_zathura
+install_kitty
+
+echo -e "\nInstalación completa."
+
  
